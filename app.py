d828ac38b216046dfa097183855f8b28e76233c9
Davi Esmeraldo <84456041+Davi-Esmeraldo@users.noreply.github.com>
2025-06-19 19:28:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ def visualizar_anotacoes_manuaais(numero_portaria):
     """
     html = style + html
 
-    components.html(html, height=300, scrolling=False)
+    components.html(html, height=350, scrolling=False)
 
 
 
@@ -113,7 +113,7 @@ def visualizar_entidades_preditas(numero_portaria):
     """
     html = style + html
 
-    components.html(html, height=300, scrolling=False)
+    components.html(html, height=350, scrolling=False)
 
 def encontrar_similares(numero_desejado, vetores_fasttext, todas_portarias_maio, top_n=10):
     if numero_desejado not in vetores_fasttext:
